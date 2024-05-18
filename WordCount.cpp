2f3a107f61143b2be847b3b87f14ae2180b8caa3
lzknot <1726690868@qq.com>
2024-05-18 12:44:43 +0800

    wrote a new c file

--- WordCount.cpp
+++ WordCount.cpp
@@ -5,37 +5,47 @@
 #define MAX_SIZE 1000
 
 int main(int argc, char *argv[]) {
+    // 检查是否提供了正确数量的命令行参数
     if(argc != 3) {
-        printf("Usage: WordCount [-c | -w] [input_file_name]\n");
+        printf("用法: WordCount [-c | -w] [input_file_name]\n");
         return 1;
     }
 
+    // 提取命令行参数
     char *parameter = argv[1];
     char *filename = argv[2];
+    
+    // 打开输入文件
     FILE *file = fopen(filename, "r");
     if(file == NULL) {
-        printf("Error opening file.\n");
+        printf("打开文件时出错。\n");
         return 1;
     }
 
     char buffer[MAX_SIZE];
     int count = 0;
 
+    // 如果参数是“-c”，则计算字符数
     if(strcmp(parameter, "-c") == 0) {
         while(fgets(buffer, MAX_SIZE, file) != NULL) {
             count += strlen(buffer);
         }
         printf("字符数：%d\n", count);
-    } else if(strcmp(parameter, "-w") == 0) {
+    } 
+    // 如果参数是“-w”，则计算单词数
+    else if(strcmp(parameter, "-w") == 0) {
         while(fscanf(file, "%s", buffer) == 1) {
             count++;
         }
         printf("单词数：%d\n", count);
-    } else {
-        printf("Invalid parameter. Please use -c or -w.\n");
+    } 
+    // 参数无效
+    else {
+        printf("无效参数。请使用 -c 或 -w。\n");
         return 1;
     }
 
+    // 关闭文件
     fclose(file);
 
     return 0;
